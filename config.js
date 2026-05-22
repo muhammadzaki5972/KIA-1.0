e558870123e1d14467c16365018454426627c6f0
Muhammad Zaki <134141036+muhammadzaki5972@users.noreply.github.com>
2026-05-22 22:37:36 +0700

    Add config.js with Google Apps Script API URL

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+// Ganti dengan URL Web App Google Apps Script Anda
+const API_URL = "https://script.google.com/macros/s/AKfycbzuNAyd9mktydkZYbq6Bbu6_J45gmr3k_3osak3pnAiRtaPEPnESM7vrpztjNr17CtJ/exec";

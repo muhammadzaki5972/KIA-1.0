2137b066be05dfb7e201ccd3991937e4f39c7d3a
Muhammad Zaki <134141036+muhammadzaki5972@users.noreply.github.com>
2026-05-22 23:36:34 +0700

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// config.js
-// Ganti dengan URL Web App Google Apps Script Anda
-const API_URL = "https://script.google.com/macros/s/AKfycbyMs3T7Svj21LRq9GkbNMmTA31c1rtsWbd8h_c9_DMQ9aqmpVA7aKuMZc7B-lqtQoo/exec";
